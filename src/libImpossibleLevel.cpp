8810c3006da0449ead2e14a02b41ad7762c8657d
MysticAx0lotl <69944483+MysticAx0lotl@users.noreply.github.com>
2024-11-05 11:08:31 -0700

    Handle loading of empty/nonexistent file

--- src/libImpossibleLevel.cpp
+++ src/libImpossibleLevel.cpp
@@ -88,165 +88,172 @@ levelObj::levelObj(char const* filename)
 void levelObj::loadDataFromFile(char const* filepath)
 {
     int currentByte = 0;
-    std::vector<unsigned char> *level;
     std::vector<unsigned char> temp = ReadAllBytes(filepath);
-    level = &temp;
-    std::cout << "getting file format version" << std::endl;
-    formatVer = readIntFromJava(*level, currentByte);
-    currentByte += 4;
-    std::cout << "Format version " << formatVer << std::endl;
-    std::cout << "Checking if special graphics are enabled" << std::endl;
-    customGraphicsEnabled = static_cast<bool>(level->at(currentByte));
-    if(customGraphicsEnabled)
+    if (sizeOf(temp) == 0)
     {
-        std::cout << "Custom graphics enabled" << std::endl;
+        cout << "Loaded empty file, data will not be processed!" << endl;
     }
-    currentByte += 1;
-    std::cout << "Attempting to read block count" << std::endl;
-    numBlocks = readShortFromJava(*level, currentByte);
-    std::cout << "There are " << numBlocks << " blocks in the level" << std::endl;
-    currentByte += 2;
-
-    blockObj *currentBlock = new blockObj;
-
-    for(int i = 0; i < numBlocks; i++)
+    else
     {
-        currentBlock->objType = static_cast<int>(level->at(currentByte));
-        std::cout << "The current block type is " << objNames[currentBlock->objType] << std::endl;
-        currentByte++;
-
-        currentBlock->xPos = readIntFromJava(*level, currentByte);
-        std::cout << "The current block's xpos is " << currentBlock->xPos << std::endl;
+        std::vector<unsigned char> *level; 
+        level = &temp;
+        std::cout << "getting file format version" << std::endl;
+        formatVer = readIntFromJava(*level, currentByte);
         currentByte += 4;
-
-        currentBlock->yPos = readIntFromJava(*level, currentByte);
-        std::cout << "The current block's ypos is " << currentBlock->yPos << std::endl;
+        std::cout << "Format version " << formatVer << std::endl;
+        std::cout << "Checking if special graphics are enabled" << std::endl;
+        customGraphicsEnabled = static_cast<bool>(level->at(currentByte));
+        if(customGraphicsEnabled)
+        {
+            std::cout << "Custom graphics enabled" << std::endl;
+        }
+        currentByte += 1;
+        std::cout << "Attempting to read block count" << std::endl;
+        numBlocks = readShortFromJava(*level, currentByte);
+        std::cout << "There are " << numBlocks << " blocks in the level" << std::endl;
+        currentByte += 2;
+    
+        blockObj *currentBlock = new blockObj;
+    
+        for(int i = 0; i < numBlocks; i++)
+        {
+            currentBlock->objType = static_cast<int>(level->at(currentByte));
+            std::cout << "The current block type is " << objNames[currentBlock->objType] << std::endl;
+            currentByte++;
+    
+            currentBlock->xPos = readIntFromJava(*level, currentByte);
+            std::cout << "The current block's xpos is " << currentBlock->xPos << std::endl;
+            currentByte += 4;
+    
+            currentBlock->yPos = readIntFromJava(*level, currentByte);
+            std::cout << "The current block's ypos is " << currentBlock->yPos << std::endl;
+            currentByte += 4;
+    
+            currentBlock->indexInVec = i;
+            std::cout << "This block can be found at index " << currentBlock->indexInVec << std::endl;
+            blockObjs->push_back(*currentBlock);
+    
+            std::cout << "Loaded object successfully!" << std::endl;
+        }
+    
+        std::cout << "Loaded " << blockObjs->size() << " object(s)!" << std::endl;
+        delete currentBlock;
+    
+        endWallPos = readIntFromJava(*level, currentByte);
+        std::cout << "End wall is located at X position " << endWallPos << std::endl;
         currentByte += 4;
-
-        currentBlock->indexInVec = i;
-        std::cout << "This block can be found at index " << currentBlock->indexInVec << std::endl;
-        blockObjs->push_back(*currentBlock);
-
-        std::cout << "Loaded object successfully!" << std::endl;
+    
+        std::cout << "Attempting to read color change count" << std::endl;
+        numBgSwitch = readIntFromJava(*level, currentByte);
+        std::cout << "There are " << numBgSwitch << " color triggers in the level" << std::endl;
+        currentByte += 4;
+    
+        bgCon *currentBg = new bgCon;
+    
+        for(int i = 0; i < numBgSwitch; i++)
+        {
+            currentBg->xPos = readIntFromJava(*level, currentByte);
+            std::cout << "The current color trigger's xpos is " << currentBg->xPos << std::endl;
+            currentByte += 4;
+    
+            currentBg->customGraphics = static_cast<bool>(level->at(currentByte));
+            currentByte++; //WILL BREAK HERE IF CUSTOM GRAPHICS ARE ENABLED FOR SOME REASON
+    
+            currentBg->colorID = readIntFromJava(*level, currentByte);
+            std::cout << "The current color type is " << colorNames[currentBg->colorID] << std::endl;
+            currentByte += 4;
+    
+            currentBg->indexInVec = i;
+    
+            std::cout << "This color trigger can be found at index " << currentBg->indexInVec << std::endl;
+            backgroundSwitches->push_back(*currentBg);
+    
+            std::cout << "Loaded color trigger successfully!" << std::endl;
+        }
+    
+        std::cout << "Loaded " << backgroundSwitches->size() << " color trigger(s)!" << std::endl;
+        delete currentBg;
+    
+        std::cout << "Attempting to read gravity change count" << std::endl;
+        numGravitySwitch = readIntFromJava(*level, currentByte);
+        std::cout << "There are " << numGravitySwitch << " gravity changes in the level" << std::endl;
+        currentByte += 4;
+    
+        gravityChange *currentGrav = new gravityChange;
+    
+        for(int i = 0; i < numGravitySwitch; i++)
+        {
+            currentGrav->xPos = readIntFromJava(*level, currentByte);
+            std::cout << "The current gravity trigger's xpos is " << currentGrav->xPos << std::endl;
+            currentByte += 4;
+    
+            currentGrav->indexInVec = i;
+    
+            std::cout << "This gravity trigger can be found at index " << currentGrav->indexInVec << std::endl;
+            gravitySwitches->push_back(*currentGrav);
+    
+            std::cout << "Loaded gravity trigger successfully!" << std::endl;
+        }
+    
+        std::cout << "Loaded " << gravitySwitches->size() << " gravity trigger(s)!" << std::endl;
+        delete currentGrav;
+    
+        std::cout << "Attempting to read falling block count" << std::endl;
+        numFallingBlocks = readIntFromJava(*level, currentByte);
+        std::cout << "There are " << numFallingBlocks << " falling blocks in the level" << std::endl;
+        currentByte += 4;
+    
+        fallingBlocks *currentFalling = new fallingBlocks;
+    
+        for(int i = 0; i < numFallingBlocks; i++)
+        {
+            currentFalling->startX = readIntFromJava(*level, currentByte);
+            std::cout << "The current falling block startX is " << currentFalling->startX << std::endl;
+            currentByte += 4;
+    
+            currentFalling->endX = readIntFromJava(*level, currentByte);
+            std::cout << "The current falling block endX is " << currentFalling->endX << std::endl;
+            currentByte += 4;
+    
+            currentFalling->indexInVec = i;
+    
+            std::cout << "This falling block section can be found at index " << currentFalling->indexInVec << std::endl;
+            fallingSections->push_back(*currentFalling);
+    
+            std::cout << "Loaded falling block section successfully!" << std::endl;
+        }
+    
+        std::cout << "Loaded " << fallingSections->size() << " falling section(s)!" << std::endl;
+        delete currentFalling;
+    
+        std::cout << "Attempting to read rising block count" << std::endl;
+        numRisingBlocks = readIntFromJava(*level, currentByte);
+        std::cout << "There are " << numRisingBlocks << " rising blocks in the level" << std::endl;
+        currentByte += 4;
+    
+        risingBlocks *currentRising = new risingBlocks;
+    
+        for(int i = 0; i < numFallingBlocks; i++)
+        {
+            currentRising->startX = readIntFromJava(*level, currentByte);
+            std::cout << "The current rising block startX is " << currentRising->startX << std::endl;
+            currentByte += 4;
+    
+            currentRising->endX = readIntFromJava(*level, currentByte);
+            std::cout << "The current rising block endX is " << currentRising->endX << std::endl;
+            currentByte += 4;
+    
+            currentRising->indexInVec = i;
+    
+            std::cout << "This rising block section can be found at index " << currentRising->indexInVec << std::endl;
+            risingSections->push_back(*currentRising);
+    
+            std::cout << "Loaded rising block section successfully!" << std::endl;
+        }
+    
+        std::cout << "Loaded " << risingSections->size() << " rising section(s)!" << std::endl;
+        delete currentRising;
     }
-
-    std::cout << "Loaded " << blockObjs->size() << " object(s)!" << std::endl;
-    delete currentBlock;
-
-    endWallPos = readIntFromJava(*level, currentByte);
-    std::cout << "End wall is located at X position " << endWallPos << std::endl;
-    currentByte += 4;
-
-    std::cout << "Attempting to read color change count" << std::endl;
-    numBgSwitch = readIntFromJava(*level, currentByte);
-    std::cout << "There are " << numBgSwitch << " color triggers in the level" << std::endl;
-    currentByte += 4;
-
-    bgCon *currentBg = new bgCon;
-
-    for(int i = 0; i < numBgSwitch; i++)
-    {
-        currentBg->xPos = readIntFromJava(*level, currentByte);
-        std::cout << "The current color trigger's xpos is " << currentBg->xPos << std::endl;
-        currentByte += 4;
-
-        currentBg->customGraphics = static_cast<bool>(level->at(currentByte));
-        currentByte++; //WILL BREAK HERE IF CUSTOM GRAPHICS ARE ENABLED FOR SOME REASON
-
-        currentBg->colorID = readIntFromJava(*level, currentByte);
-        std::cout << "The current color type is " << colorNames[currentBg->colorID] << std::endl;
-        currentByte += 4;
-
-        currentBg->indexInVec = i;
-
-        std::cout << "This color trigger can be found at index " << currentBg->indexInVec << std::endl;
-        backgroundSwitches->push_back(*currentBg);
-
-        std::cout << "Loaded color trigger successfully!" << std::endl;
-    }
-
-    std::cout << "Loaded " << backgroundSwitches->size() << " color trigger(s)!" << std::endl;
-    delete currentBg;
-
-    std::cout << "Attempting to read gravity change count" << std::endl;
-    numGravitySwitch = readIntFromJava(*level, currentByte);
-    std::cout << "There are " << numGravitySwitch << " gravity changes in the level" << std::endl;
-    currentByte += 4;
-
-    gravityChange *currentGrav = new gravityChange;
-
-    for(int i = 0; i < numGravitySwitch; i++)
-    {
-        currentGrav->xPos = readIntFromJava(*level, currentByte);
-        std::cout << "The current gravity trigger's xpos is " << currentGrav->xPos << std::endl;
-        currentByte += 4;
-
-        currentGrav->indexInVec = i;
-
-        std::cout << "This gravity trigger can be found at index " << currentGrav->indexInVec << std::endl;
-        gravitySwitches->push_back(*currentGrav);
-
-        std::cout << "Loaded gravity trigger successfully!" << std::endl;
-    }
-
-    std::cout << "Loaded " << gravitySwitches->size() << " gravity trigger(s)!" << std::endl;
-    delete currentGrav;
-
-    std::cout << "Attempting to read falling block count" << std::endl;
-    numFallingBlocks = readIntFromJava(*level, currentByte);
-    std::cout << "There are " << numFallingBlocks << " falling blocks in the level" << std::endl;
-    currentByte += 4;
-
-    fallingBlocks *currentFalling = new fallingBlocks;
-
-    for(int i = 0; i < numFallingBlocks; i++)
-    {
-        currentFalling->startX = readIntFromJava(*level, currentByte);
-        std::cout << "The current falling block startX is " << currentFalling->startX << std::endl;
-        currentByte += 4;
-
-        currentFalling->endX = readIntFromJava(*level, currentByte);
-        std::cout << "The current falling block endX is " << currentFalling->endX << std::endl;
-        currentByte += 4;
-
-        currentFalling->indexInVec = i;
-
-        std::cout << "This falling block section can be found at index " << currentFalling->indexInVec << std::endl;
-        fallingSections->push_back(*currentFalling);
-
-        std::cout << "Loaded falling block section successfully!" << std::endl;
-    }
-
-    std::cout << "Loaded " << fallingSections->size() << " falling section(s)!" << std::endl;
-    delete currentFalling;
-
-    std::cout << "Attempting to read rising block count" << std::endl;
-    numRisingBlocks = readIntFromJava(*level, currentByte);
-    std::cout << "There are " << numRisingBlocks << " rising blocks in the level" << std::endl;
-    currentByte += 4;
-
-    risingBlocks *currentRising = new risingBlocks;
-
-    for(int i = 0; i < numFallingBlocks; i++)
-    {
-        currentRising->startX = readIntFromJava(*level, currentByte);
-        std::cout << "The current rising block startX is " << currentRising->startX << std::endl;
-        currentByte += 4;
-
-        currentRising->endX = readIntFromJava(*level, currentByte);
-        std::cout << "The current rising block endX is " << currentRising->endX << std::endl;
-        currentByte += 4;
-
-        currentRising->indexInVec = i;
-
-        std::cout << "This rising block section can be found at index " << currentRising->indexInVec << std::endl;
-        risingSections->push_back(*currentRising);
-
-        std::cout << "Loaded rising block section successfully!" << std::endl;
-    }
-
-    std::cout << "Loaded " << risingSections->size() << " rising section(s)!" << std::endl;
-    delete currentRising;
 
     std::cout << "Loaded entire level!" << std::endl;
 }
