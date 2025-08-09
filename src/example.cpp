892db283f70ddeac0a491cf059faa4168f68b727
MysticAx0lotl <69944483+MysticAx0lotl@users.noreply.github.com>
2025-08-09 17:37:00 -0600

    fix a stupid mistake

--- src/example.cpp
+++ src/example.cpp
@@ -3,31 +3,10 @@
 //This is an example that generates a blank level
 int main()
 {
-    Level leveldata("test.lvl", true);
-    
-    BlockObject* test1 = new BlockObject;
-    test1->xPos = 590;
-    test1->yPos = 650;
-    test1->objType = 02;
+    Level leveldata("heaven.lvl", true);
 
-    BlockObject* test2 = new BlockObject;
-    test2->xPos = 500;
-    test2->yPos = 560;
-    test2->objType = 02;
+    Level leveldata2("corrupt.lvl", true);
 
-    leveldata.addBlock(test1);
-    leveldata.addBlock(test2);
-
-    leveldata.printSummary();
-
-    leveldata.saveLevel("test2.lvl");
-
-    Level level2("test2.lvl", true);
-    level2.mergeAdjacentPits();
-
-    level2.printSummary();
-
-    level2.saveLevel("test3.lvl");
 
     return 0;
 }
