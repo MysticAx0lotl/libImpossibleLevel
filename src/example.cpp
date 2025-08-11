be34025ab484948fbfa4c8aef71100cc61de83d6
MysticAx0lotl <69944483+MysticAx0lotl@users.noreply.github.com>
2025-08-10 21:17:33 -0600

    Finish mergeAdjacentPits
    Should be functionally equivalent to the real TIG editor backend now

--- src/example.cpp
+++ src/example.cpp
@@ -3,10 +3,30 @@
 //This is an example that generates a blank level
 int main()
 {
-    Level leveldata("heaven.lvl", true);
+    Level leveldata(false);
+    
+    BlockObject* testPit = new BlockObject;
+    *testPit = {30, 60, 02, 0};
+    BlockObject* testPit2 = new BlockObject;
+    *testPit2 = {40, 90, 02, 0};
+    leveldata.addBlock(testPit);
+    leveldata.addBlock(testPit2);
 
-    Level leveldata2("corrupt.lvl", true);
+    BlockObject* testPit3 = new BlockObject;
+    *testPit3 = {10, 30, 02, 0};
+    BlockObject* testPit4 = new BlockObject;
+    *testPit4 = {40, 120, 02, 0};
+    leveldata.addBlock(testPit3);
+    leveldata.addBlock(testPit4);
 
+    leveldata.printSummary();
+
+    leveldata.mergeAdjacentPits(false);
+
+    leveldata.printSummary();
+
+    std::cout << leveldata.getBlockAtIndex(0)->xPos << std::endl;
+    std::cout << leveldata.getBlockAtIndex(0)->yPos << std::endl;
 
     return 0;
 }
