60bd36aa809ecf7e60f6a3ad22fb7c13378adf4e
MysticAx0lotl <69944483+MysticAx0lotl@users.noreply.github.com>
2025-08-18 11:22:05 -0600

    change example

--- src/example.cpp
+++ src/example.cpp
@@ -3,30 +3,9 @@
 //This is an example that generates a blank level
 int main()
 {
-    Level leveldata(false);
-    
-    BlockObject* testPit = new BlockObject;
-    *testPit = {30, 60, 02, 0};
-    BlockObject* testPit2 = new BlockObject;
-    *testPit2 = {40, 90, 02, 0};
-    leveldata.addBlock(testPit);
-    leveldata.addBlock(testPit2);
-
-    BlockObject* testPit3 = new BlockObject;
-    *testPit3 = {10, 30, 02, 0};
-    BlockObject* testPit4 = new BlockObject;
-    *testPit4 = {40, 120, 02, 0};
-    leveldata.addBlock(testPit3);
-    leveldata.addBlock(testPit4);
+    Level leveldata("phazd_xbox.lvl", false);
 
     leveldata.printSummary();
-
-    leveldata.mergeAdjacentPits(false);
-
-    leveldata.printSummary();
-
-    std::cout << leveldata.getBlockAtIndex(0)->xPos << std::endl;
-    std::cout << leveldata.getBlockAtIndex(0)->yPos << std::endl;
 
     return 0;
 }
